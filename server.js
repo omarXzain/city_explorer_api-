70ee781c092ed06620cc5e4013945a4f5630920b
Omar Zain <omaren2010@gmail.com>
2020-10-25 22:35:22 +0300

    added weather

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-let express = require('express');
-let cors = require('cors');
-const { response, json } = require('express');
-
-let app = express();
+'use strict';
+/* eslint-disable no-undef */
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT;
 app.use(cors());
 
-require('dotenv').config();
-
-const PORT = process.env.PORT;
+const { response, json } = require('express');
 
 app.get('/location', handleLocation);
 
-function handleLocation(req,res){
+function handleLocation(req, res){
   let city = req.query.city;
   let jsonData = require('./data/location.json');
   let jsonObject = jsonData[0];
@@ -19,6 +19,7 @@ function handleLocation(req,res){
   res.status(200).json(locationObject);
 
 }
+
 function Location(search_query, formatted_query, latitude, longitude){
   this.search_query = search_query;
   this.formatted_query = formatted_query;
@@ -31,7 +32,38 @@ function Location(search_query, formatted_query, latitude, longitude){
 //   "latitude": "47.606210",
 //   "longitude": "-122.332071"
 // }
+
+
+
+//===========================================
+
+app.get('/weather', handleWeather);
+
+
+
+function handleWeather(req, res){
+  let dataArr = [];
+  let jsonData = require('./data/weather.json');
+
+  try{
+    for(let i=0; i<jsonData.data.length; i++){
+      let locationObject = new Weather(jsonData.data[i].weather.description, jsonData.data[i].valid_date);
+      dataArr.push(locationObject);
+    }
+    res.status(200).json(dataArr);
+  } catch (error) {
+    res.status(500).send('something went wrong!');
+  }
+
+}
+function Weather(forecast, time){
+  this.forecast = forecast;
+  this.time = time;
+}
+
+
+
+
 app.listen(PORT, ()=>{
   console.log(`app is listening on port ${PORT}`);
 });
-
