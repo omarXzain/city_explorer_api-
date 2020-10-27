7b2d47ca94ab3a404e283dcc7dfa1d6eec0e04f8
Omar Zain <omaren2010@gmail.com>
2020-10-27 10:04:56 +0300

    lab8

--- server.js
+++ server.js
@@ -2,13 +2,16 @@
 /* eslint-disable no-redeclare */
 /* eslint-disable no-undef */
 require('dotenv').config();
+let pg = require('pg');
 const express = require('express');
 const app = express();
 const superagent = require('superagent');
 const cors = require('cors');
 const PORT = process.env.PORT;
-app.use(cors());
+const DATABASE_URL = process.env.DATABASE_URL;
+let client = new pg.Client(DATABASE_URL);
 
+app.use(cors());
 app.get('/location', handleLocation);
 
 function handleLocation(req, res) {
@@ -99,6 +102,48 @@ function Trails(trailData) {
 }
 
 
-app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`);
+
+//=============================================//
+function handleGettingLocations(req, res) {
+  // get the data from the database ...
+  client.query('SELECT * FROM Locations;').then(data => {
+    res.send(data.rows);
+  }).catch(err => {
+    res.send('sorry .. an error occured ...', err);
+  });
+}
+
+function handleAddingLocations(req, res) {
+  const city = request.query.city;
+  location = new Location(city, locationData.body[0]);
+  let selecCity = 'SELECT * FROM location WHERE search_query = $1;';
+  let cityVal = [city];
+  client.query(selecCity, cityVal).then(result => {
+
+    response.status(200).json(result.rows);
+
+    let newLocation = 'INSERT INTO Locations(search_query,formatted_query,latitude,longitude) values ($1,$2,$3,$4) RETURNING *;';
+    const ValuesInsert = [city, location.formatted_query, location.latitude, location.longitude];
+
+    client.query(newLocation, ValuesInsert).then(DataX => {
+      response.status(200).json(DataX);
+    }).catch(() => {
+      response.status(500).send('Something Went Wrong');
+    });
+
+  }
+}
+function handleWrongPaths(req, res) {
+  res.status(404).send('page not found');
+}
+
+
+//==============================================//
+client.connect().then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening to port ${PORT}`);
+  });
+}).catch(err => {
+  console.log('Sorry ... and error occured ..', err);
 });
+
